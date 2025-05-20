371245fd8d4c730707e2e76a38315a78ccdbb00a
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-05-20 23:20:28 +0530

    Update logger.py

--- logging/logger.py
+++ logging/logger.py
@@ -11,7 +11,6 @@ def setup_logger(name, log_file, level=logging.INFO):
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
 
-
     if not logger.handlers:
         logger.addHandler(file_handler)
 
